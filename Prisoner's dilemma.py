da35ffd11cac61aa9715f0dcb8b2b3fc10122758
w50111831 <W50111831@STU.worthing.ac.uk>
2025-09-29 20:40:52 +0100

    Add computer decision making controls

--- Prisoner's dilemma.py
+++ Prisoner's dilemma.py
@@ -4,6 +4,16 @@ computerpoints = 0
 extrauserpoints = 0
 extracomputerpoints = 0
 
+def pick_strategy():
+    print("Choose the computer's strategy")
+    print("1:Random")
+    print("2:defect")
+    print("3:cooperate")
+    print("4:copyuser")
+    strategy = input("Enter choice 1-4")
+    return strategy
+
+
 def choose():
     textchoice = input("would you like to Co-operate (a) or Defect (d)")
     if textchoice == "a":
@@ -30,18 +40,27 @@ def givepoints(choice1, choice2):
     else:
         print("Game error?")
 
+def computer_move(strategy, choice1):
+    if strategy == "1":
+        if random.randint(1,2) == 1:
+            computerchoice = True
+        else:
+            computerchoice = False
+    if strategy == "2":
+        computerchoice = False
+    if strategy == "3":
+        computerchoice = True
+    if strategy == "4":
+        computerchoice = choice1
+    return computerchoice
 
 
-
+strategy = pick_strategy()
 
 #MAIN GAME LOOP
-
 while True:
     choice1 = choose()
-    if random.randint(1,2) == 1:
-        computerchoice = True
-    else:
-        computerchoice = False
+    computerchoice = computer_move(strategy, choice1)
     extrauserpoints, extracomputerpoints = givepoints(choice1, computerchoice)
     userpoints += extrauserpoints
     computerpoints += extracomputerpoints
